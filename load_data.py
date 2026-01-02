5d339892541449ca134e5e9db1e25007e1889a3e
Destiny Newman <destinynewman79@gmail.com>
2026-01-02 12:51:37 +0000

    extracts close prices and prints numpy.ndarray

--- load_data.py
+++ load_data.py
@@ -1,8 +1,30 @@
 import csv
+import numpy as np
 
-with open('data.txt', 'r') as file:
-    data = file.read()
-    close = csv.reader(data.splitlines())
-    for row in close:
-        if len(row) >= 2:
-            print(row[-2])
+
+def create_close_price_array():
+    with open('/home/destiny/Programming/Kraken_OHLCVT/1INCHEUR_1440.csv', 'r') as file:
+        data = file.read()
+        close = csv.reader(data.splitlines())
+        for row in close:
+        #print only the closing prices
+            #print(row[4])
+       #save to a seperate file
+            with open('/home/destiny/Programming/close_prices.csv', 'a') as close_file:
+                close_writer = csv.writer(close_file)
+                close_writer.writerow([row[4]])
+     
+def create_close_price_numpy_array():
+    close_prices = np.loadtxt('/home/destiny/Programming/close_prices.csv', delimiter=',')
+    np.save('/home/destiny/Programming/close_prices.npy', close_prices)
+    print(close_prices)
+    return close_prices
+
+
+if __name__ == "__main__":
+    create_close_price_array()
+    create_close_price_numpy_array()
+
+
+
+        
