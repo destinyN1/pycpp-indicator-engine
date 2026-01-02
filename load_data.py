6d4124c7fd28d96b1819aedde79cddd619d547ec
Destiny Newman <destinynewman79@gmail.com>
2026-01-02 12:02:25 +0000

    some base code in loaddata.py

--- load_data.py
+++ load_data.py
@@ -1,1 +1,8 @@
+import csv
 
+with open('data.txt', 'r') as file:
+    data = file.read()
+    close = csv.reader(data.splitlines())
+    for row in close:
+        if len(row) >= 2:
+            print(row[-2])
