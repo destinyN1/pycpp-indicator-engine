9c9e37c530bbf78de41d98acc83cdf42641ed689
destinyN1 <d.newman7@universityofgalway.ie>
2026-01-03 21:03:48 +0000

    Fix variable name for close prices in main block

--- indicators.py
+++ indicators.py
@@ -58,8 +58,8 @@ def compare_lengths_sma_close_prices(close_prices, sma):
 
 
 if __name__ == "__main__":
-    close_prices_averaged = import_close_prices('/home/destiny/Programming/pycpp-indicator-engine/close_prices.npy')
-    sma_fast= simple_moving_average(close_prices, 10)
+    close_prices = import_close_prices('/home/destiny/Programming/pycpp-indicator-engine/close_prices.npy') 
+    sma_fast= simple_moving_average(close_prices, 10) #sma_fast/slow, ema_fast/slow functions need to be imported into strategy.py
     sma_slow= simple_moving_average(close_prices, 100)
     ema_fast= exponential_moving_average(close_prices, 10)
     ema_slow= exponential_moving_average(close_prices, 100)
@@ -69,9 +69,9 @@ if __name__ == "__main__":
     np.save('/home/destiny/Programming/pycpp-indicator-engine/sma_slow.npy', sma_slow)
     np.save('/home/destiny/Programming/pycpp-indicator-engine/ema_slow.npy', ema_slow)
     #plot close prices vs sma and ema
-    plot_close_prices_vs_sma(close_prices_averaged, sma_fast)
-    plot_close_prices_vs_ema(close_prices_averaged, ema_fast)
-    plot_close_prices_vs_sma(close_prices_averaged, sma_slow)
-    plot_close_prices_vs_ema(close_prices_averaged, ema_slow)
+    plot_close_prices_vs_sma(close_prices, sma_fast)
+    plot_close_prices_vs_ema(close_prices, ema_fast)
+    plot_close_prices_vs_sma(close_prices, sma_slow)
+    plot_close_prices_vs_ema(close_prices, ema_slow)
     #compare_lengths_sma_close_prices(close_prices_averaged, sma_10)
     ##print("Simple Moving Average (10):", sma_10)
