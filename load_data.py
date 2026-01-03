2558df8546f0328d38c05d39d773f1b5f1234913
destinyN1 <d.newman7@universityofgalway.ie>
2026-01-03 20:52:22 +0000

    Add comments to load_data.py functions
    Added comments to clarify variable paths and function usage.

--- load_data.py
+++ load_data.py
@@ -2,7 +2,7 @@ import csv
 import numpy as np
 
 
-def create_close_price_array():
+def create_close_price_array(): #put variable path here
     with open('/home/destiny/Programming/Kraken_OHLCVT/1INCHEUR_1440.csv', 'r') as file:
         data = file.read()
         close = csv.reader(data.splitlines())
@@ -14,7 +14,7 @@ def create_close_price_array():
                 close_writer = csv.writer(close_file)
                 close_writer.writerow([row[4]])
      
-def create_close_price_numpy_array():
+def create_close_price_numpy_array(): #this function needs to be imported into the next part of the flow
     close_prices = np.loadtxt('/home/destiny/Programming/close_prices.csv', delimiter=',')
     np.save('/home/destiny/Programming/close_prices.npy', close_prices)
     print(close_prices)
